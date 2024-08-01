c37b4e9efde206b54ea4bcfde814176c57ab1b34
Nandan <nandankumar1475@gmail.com>
2024-08-01 16:48:08 +0530

    setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
+app.post('/todo' ,function(req,res){
+
+});
+
+app.get('/gettodo',(req,res)=>{
+
+});
+
+app.put('/completed',(req,res)=>{
+
+});
